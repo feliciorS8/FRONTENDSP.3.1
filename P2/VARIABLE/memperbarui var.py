3e3f492cacb8e1d5a20557523b8659144cdd7957
feliciorS8 <onlydija33@gmail.com>
2025-10-28 10:50:09 +0700

    Add files via upload

---
+++ P2/VARIABLE/memperbarui var.py
@@ -0,0 +1,11 @@
+age = 20
+#lalu diperbarui
+age = 30
+print(age)
+
+
+#bookTitle = "Learn Python Programming"
+#delete bookTitle 
+#bookTitle = "Learn"  # Variabel dibuat
+#del bookTitle        # Variabel dihapus (Syntax Benar)
+#print(bookTitle)     # Baris ini akan menyebabkan NameError karena bookTitle sudah dihapus
